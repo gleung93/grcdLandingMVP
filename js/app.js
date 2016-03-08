8de90c751a4c8536ca42678dc4f1c41350f7be03
gleung93 <grantleung@grants-mbp.daap.uc.edu>
2016-03-08 15:07:52 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//console.log("loaded");
+
+Draggable.create(".drag", {
+	throwProps: true,
+	throwResistance: "2000",
+	bounds: document.getElementById("boundaries"),
+});
+
+//var floater = document.getElementByClassName("focus");
+//TweenLite.to(".focus", 10, {top:"0%"});
